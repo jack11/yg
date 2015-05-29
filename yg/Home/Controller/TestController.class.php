6c3fb1d7b547e381efe65cff0bbb9dea9b3b6492
jack <531612615@qq.com>
2015-05-30 01:39:56 +0800

    Signed-off-by: jack <531612615@qq.com>

--- yg/Home/Controller/TestController.class.php
+++ yg/Home/Controller/TestController.class.php
@@ -14,4 +14,31 @@ class TestController extends BaseController {
 		
 	}
 	
+	public function email(){
+		$mail =  \Tools\mail\Mail::getInstance();
+		$res = $mail->send(1,2,'531612615@qq.com','php0620@163.com','3');
+		if($res){
+			echo '发送成功';
+		}else{
+			echo '发送失败';
+		}
+	}
+	
+	public function fenci(){
+		$fenci = new \Tools\Fenci\Fenci();
+		$teststr = '我们是共产党我们';
+
+		$fenci->SetSource($teststr);
+		
+		//设置分词属性
+		$fenci -> resultType = 2;
+		$fenci -> differMax = true;
+	
+		$fenci -> StartAnalysis();
+	
+		echo '<pre>';
+		//获取你想要的结果
+		print_r($fenci -> GetFinallyIndex());
+	}
+
 }
